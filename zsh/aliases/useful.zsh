440748ab060499ee127a9aeb3287d639de2e1994
Sricharan AR <arsricharan1999@gmail.com>
2020-07-08 13:06:20 +0530

    Back to familiar shutdown hotkey

--- zsh/aliases/useful.zsh
+++ zsh/aliases/useful.zsh
@@ -1,5 +1,5 @@
 alias sniptex="vim ~/.vim/UltiSnips/tex.snippets"
 alias alias_add="vim $ZSH/custom/aliases.zsh"
-alias tex_clean="rm *.log *.gz *.fls *.fdb_latexmk *.aux"
+alias tex_clean="rm *.log *.gz *.fls *.fdb_latexmk *.aux *.bcf *.blg *.xml *.bbl"
 alias other="cd /hdd/home/cs"
 
