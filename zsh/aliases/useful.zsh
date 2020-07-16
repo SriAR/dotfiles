c38172543212b26f93e1dd43e2276213869786b0
Sricharan AR <arsricharan1999@gmail.com>
2020-07-16 11:53:37 +0530

    Better tex_clean

--- zsh/aliases/useful.zsh
+++ zsh/aliases/useful.zsh
@@ -1,5 +1,5 @@
 alias sniptex="vim ~/.vim/UltiSnips/tex.snippets"
 alias alias_add="cd /home/cs/dotfiles/zsh/"
-alias tex_clean="rm *.log *.gz *.fls *.fdb_latexmk *.aux *.bcf *.blg *.xml *.bbl"
+alias tex_clean='find . -type f -regextype egrep -regex "(.*)(\.aux|\.bbl|\.blg|\-blx\.bib|\.fdb_latexmk|\.fls|\.log|\.run\.xml|\.synctex\.gz)$" -delete'
 alias other="cd /hdd/home/cs"
 
