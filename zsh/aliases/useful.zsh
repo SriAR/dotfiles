1e340d6a57671b56456ba11987fa0f27ad1ae326
Sricharan AR <arsricharan1999@gmail.com>
2020-10-25 12:37:41 +0530

    new tex clean

--- zsh/aliases/useful.zsh
+++ zsh/aliases/useful.zsh
@@ -1,4 +1,4 @@
 alias sniptex="vim ~/.vim/UltiSnips/tex.snippets"
 alias alias_add="cd /home/cs/dotfiles/zsh/"
-alias tex_clean='find . -type f -regextype egrep -regex "(.*)(\.aux|\.bbl|\.blg|\-blx\.bib|\.fdb_latexmk|\.fls|\.log|\.run\.xml|\.synctex\.gz)$" -delete'
-alias other="sudo mount /dev/sda6 /hdd && cd /hdd/home/cs"
+alias tex_clean='find . -type f -regextype egrep -regex "(.*)(\.aux|\.bbl|\.out|\.blg|\-blx\.bib|\.fdb_latexmk|\.fls|\.log|\.run\.xml|\.synctex\.gz)$" -delete'
+alias other="ranger /hdd/home/cs/Music"
