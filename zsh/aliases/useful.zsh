198148cfad0a5ba3276a3f2c27b3691095335729
Sricharan AR <arsricharan1999@gmail.com>
2020-10-25 12:42:58 +0530

    updating zinc

--- zsh/aliases/useful.zsh
+++ zsh/aliases/useful.zsh
@@ -1,4 +1,4 @@
 alias sniptex="vim ~/.vim/UltiSnips/tex.snippets"
 alias alias_add="cd /home/cs/dotfiles/zsh/"
-alias tex_clean='find . -type f -regextype egrep -regex "(.*)(\.aux|\.bbl|\.out|\.blg|\-blx\.bib|\.fdb_latexmk|\.fls|\.log|\.run\.xml|\.synctex\.gz)$" -delete'
+alias tex_clean='find . -type f -regextype egrep -regex "(.*)(\.aux|\.bbl|\.out|\.blg|\.brf|\-blx\.bib|\.fdb_latexmk|\.fls|\.log|\.run\.xml|\.synctex\.gz)$" -delete'
 alias other="ranger /hdd/home/cs/Music"
