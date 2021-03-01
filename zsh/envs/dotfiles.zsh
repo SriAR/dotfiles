8e7890c494bf034baf4204f2e3c0deb32534d9c8
Sricharan AR <arsricharan1999@gmail.com>
2021-03-01 10:11:11 +0530

    vim to nvim

--- zsh/envs/dotfiles.zsh
+++ zsh/envs/dotfiles.zsh
@@ -3,4 +3,5 @@ XDG_CONFIG_HOME="$HOME/.config/"
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=60'
 path+=('/home/cs/dotfiles/bin')
 path+=('/home/cs/.local/bin')
+path+=('/home/cs/.npm_packages/bin')
 export PATH
---
+++ zsh/envs/wine.zsh
@@ -0,0 +1,2 @@
+WINEPREFIX="/hdd/Games/.wine"
+
