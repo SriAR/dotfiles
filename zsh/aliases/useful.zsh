5780ed2aaf8bef4ab4d1bfe9b3632251cd1f0f7d
Sricharan AR <arsricharan1999@gmail.com>
2021-03-17 10:15:44 +0530

    added bluetooth and airplay

--- zsh/aliases/useful.zsh
+++ zsh/aliases/useful.zsh
@@ -2,3 +2,4 @@ alias sniptex="vim ~/.vim/UltiSnips/tex.snippets"
 alias alias_add="cd /home/cs/dotfiles/zsh/"
 alias tex_clean='find . -type f -regextype egrep -regex "(.*)(\.aux|\.bbl|\.out|\.blg|\.bcf|\.brf|\-blx\.bib|\.fdb_latexmk|\.fls|\.log|\.run\.xml|\.synctex\.gz)$" -delete'
 alias camera="mpv --demuxer-lavf-format=video4linux2 --demuxer-lavf-o-set=input_format=mjpeg av://v4l2:/dev/video0"
+alias airplay="systemctl start avahi-daemon && uxplay"
