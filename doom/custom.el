eb3888a3ac0fb7aeb9251282ebf734a4e79f001b
Sricharan AR <arsricharan1999@gmail.com>
2022-11-05 11:31:09 +0100

    removed meet

---
+++ doom/custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-insert-braces nil)
+ '(font-latex-math-environments
+   '("display" "displaymath" "equation" "eqnarray" "gather" "multline" "align" "alignat" "xalignat" "dmath"))
+ '(safe-local-variable-values
+   '((org-roam-db-location . "~/dynAlgLab/org-roam.db")
+     (org-roam-directory . "~/dynAlgLab/"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
