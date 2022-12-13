372a4c690d1da711d54915eac1378eef07880e09
Sricharan AR <arsricharan1999@gmail.com>
2022-12-13 15:29:12 +0100

    doomd changes

--- doom/custom.el
+++ doom/custom.el
@@ -10,9 +10,19 @@
    '((org-roam-db-location . "~/dynAlgLab/org-roam.db")
      (org-roam-directory . "~/dynAlgLab/")))
  '(warning-suppress-types '((doom-after-init-hook) (defvaralias))))
+ '(org-html-head-include-default-style nil)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(add-to-list 'org-publish-project-alist
+         '("Electrical Flows"
+        :base-directory "~/Study/Writeups/Electrical Flows"
+        :publishing-directory "~/Study/Writeups/Electrical Flows"
+        :publishing-function org-latex-publish-to-pdf
+        :body-only t
+        :makeindex t
+        ))
