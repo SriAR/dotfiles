36096286443059261979204bc148fcc6a9dd0e83
Sricharan AR <arsricharan1999@gmail.com>
2022-12-27 15:48:05 +0100

    zathura and doom

--- doom/custom.el
+++ doom/custom.el
@@ -11,6 +11,7 @@
      (org-roam-directory . "~/dynAlgLab/")))
  '(warning-suppress-types '((doom-after-init-hook) (defvaralias))))
  '(org-html-head-include-default-style nil)
+ '(org-publish-use-timestamps-flag nil)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -18,11 +19,51 @@
  ;; If there is more than one, they won't work right.
  )
 
-(add-to-list 'org-publish-project-alist
-         '("Electrical Flows"
-        :base-directory "~/Study/Writeups/Electrical Flows"
-        :publishing-directory "~/Study/Writeups/Electrical Flows"
+(setq org-publish-project-alist
+      '(("orgfiles"
+         :base-directory "~/newsite"
+         :base-extension "org"
+         :publishing-directory "~/public"
+         :publishing-function org-html-publish-to-html
+         :exclude "home/*"
+         :recursive t
+         :html-doctype "html5"
+         :html-postamble "<hr style=\"border-top: 1px;\"><p>%C</p>"
+         :html-head-include-default-style nil
+         :with-toc nil
+         :with-title nil
+         :with-author nil
+         :section-numbers nil
+         :html-head "<link rel=\"icon\" type=\"image/x-icon\" href=\"/favicon.ico\"> <link rel=\"stylesheet\" href=\"/style.css\" />"
+         :html-preamble "<div id=\"navbar\">
+    <a href=\"/\"><div class=\"navitem home\">&heartsuit;</div></a>
+    <a href=\"/pubs\"><div class=\"navitem\">pubs</div></a>
+    <a href=\"/blog\"><div class=\"navitem\">blog</div></a>
+    <a href=\"/meta\"><div class=\"navitem\">meta</div></a>
+    <a href=\"/files/Sricharan_AR_CV.pdf\"><div class=\"navitem\">cv</div></a>
+</div> <hr>
+<div class=\"sidebar\">
+    <img src=\"/files/narcissism.jpg\" alt=\"me.jpg\" />
+    <span class=\"sbitem\"><a href=\"mailto:arsricharan@cmi.ac.in\">Email</a></span>
+    <span class=\"sbitem\"><a href=\"https://github.com/SriAR\">Github</a></span>
+    <span class=\"sbitem\"><a href=\"https://twitter.com/ratheritslimit\">Twitter</a></span>
+    <span class=\"sbitem\"><a href=\"https://scholar.google.com/citations?hl=en&user=wNnXe84AAAAJ\">Google Scholar</a></span>
+</div>"
+         )
+
+        ("other"
+         :base-directory "~/newsite"
+         :base-extension "ico\\|css\\|ttf\\|woff2\\|woff\\|jpg\\|png\\|pdf"
+         :publishing-directory "~/public"
+         :recursive t
+         :publishing-function org-publish-attachment)
+
+        ("website" :components ("orgfiles" "other"))
+
+        ("eflows"
+        :base-directory "~/Study/Writeups/eflows"
+        :publishing-directory "~/Study/Writeups/eflows"
         :publishing-function org-latex-publish-to-pdf
-        :body-only t
-        :makeindex t
+        )
+
         ))
